b7dbfbdbc4607168aaa57550be9f6341845fa5f0
Dant86 <vedantdpathak@iCloud.com>
2019-11-26 16:41:16 -0600

    added threads and generates only 1 csv file'

--- scrape.py
+++ scrape.py
@@ -1,57 +1,75 @@
-import requests, csv
+import requests
+import threading
+import sys
+import os
 from bs4 import BeautifulSoup
- 
+from pandas import DataFrame
+from functools import reduce
 
-#URLS = [f"https://www.volunteermatch.org/search/?categories=3&s={s}" for s in [x*10+1 for x in range(10000)]]
-#print(URLS)
-location = "New+York%2C+NY%2C+USA"
-#category = 25 #community
-#category = 34 # arts_culture
-#category = 23 # advocacy
-#category = 12 # seniors
-#category = 7 # homeless_housing
-#category = 11 # health_med
-#category = 22 # child_youth
-#category = 30 # animals
-#category = 15 #edu_lit
-#category = 14 # crisis_support
-#category = 17 # disabilities
-category = 3 # women
-URLS = [f"https://www.volunteermatch.org/search/?categories={category}&l={location}&s={s}" for s in [x*10+1 for x in range(14)]]
+if len(sys.argv) == 1:
+    CSV_FNAME = 'volunteermatch_data.csv'
+else:
+    CSV_FNAME = sys.argv[1]
 
-opps = []
+URL_PART1 = 'https://www.volunteermatch.org/search/'
 
-for URL in URLS:
+locations = ['New+York%2C+NY%2C+USA', 'Chicago%2C+IL%2C+USA']
 
-    r = requests.get(URL) 
-    #print(r.content)
+categories = {
+    'community': 25,
+    'arts_culture': 34,
+    'advocacy': 23,
+    'seniors': 12,
+    'homeless_housing': 7,
+    'health_med': 11,
+    'child_youth': 22,
+    'animals': 30,
+    'edu_lit': 15,
+    'crisis_support': 14,
+    'women': 3
+}
 
-    soup = BeautifulSoup(r.content, 'html5lib') 
-    #print(soup.prettify())
-    table = soup.find('div', attrs = {'class':'search-page'})
-    for row in table.findAll('div', attrs = {'class':'searchitem PUBLIC'}):
-        opp = {}
-        opp['title'] = row.h3.text.strip()
-        opp['org'] = row.find_all("a")[1].text.strip()
-        desc_search = row.find_all("p")
-        desc_index = 0
-        for i in range(0,len(desc_search)):
-            #print(desc_search[i].getText())
-            if len(desc_search[i].getText()) > 100:
-                desc_index = i
-                #print(i)
-        opp['desc'] = row.find_all("p")[desc_index].getText().strip()
-        opp['desc'] = opp['desc'].split("\n")[0]
-        print(opp)
-        opps.append(opp)
+dfs = []
 
-#print(table)
+def url_to_df(urls, cat):
+    opps = []
+    for url in urls:
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, 'html5lib')
+        table = soup.find('div', attrs={'class': 'search-page'})
+        for row in table.findAll('div', attrs={'class': 'searchitem PUBLIC'}):
+            opp = {}
+            opp['title'] = row.h3.text.strip()
+            opp['org'] = row.find_all('a')[1].text.strip()
+            desc_search = row.find_all('p')
+            desc_index = 0
+            for i in range(len(desc_search)):
+                if len(desc_search[i].getText()) > 100:
+                    desc_index = i
+            opp['desc'] = row.find_all('p')[desc_index].getText().strip()
+            opp['desc'] = opp['desc'].split('\n')[0]
+            opp['cat'] = True
+            opps.append(opp)
+    print(f'scraping for {cat} done!')
+    dfs.append(DataFrame(opps))
 
+threads = []
+for cat, cat_v in categories.items():
+    urls = []
+    for loc in locations:
+        url = URL_PART1 + f'?categories={cat_v}&' \
+                        + f'l={loc}'
+        urls += [url + f'&s={s}' for s in [x*10+1 for x in range(100)]]
+    t = threading.Thread(target=url_to_df, args=(urls, cat))
+    threads.append(t)
+    t.start()
 
-with open("vmatch_scrape_women_ny.csv", "w") as f:
-    w = csv.DictWriter(f,['title','org','desc']) 
-    w.writeheader()
-    for opp in opps: 
-        w.writerow(opp) 
-f.close()
+for t in threads:
+    t.join()
+
+final_df = reduce(lambda df1, df2: df1.merge(df2, left_on=['title', 'org', 'desc'],
+                                                  right_on=['title', 'org', 'desc'],
+                                                  how='outer'), dfs)
+final_df.fillna(False)
+final_df.to_csv(CSV_FNAME)
 
